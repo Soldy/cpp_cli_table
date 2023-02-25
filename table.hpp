e5c31763a69c804cb880be04986ac97e3bacde53
Soldy <4786022+Soldy@users.noreply.github.com>
2023-02-25 19:12:41 +0000

    render helpers
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ table.hpp
@@ -0,0 +1,79 @@
+#include <vector>
+#include <string>
+#include <map>
+
+namespace ctcpp{
+class Table{
+  public:
+    std::vector<std::string> render (
+      std::vector<std::vector<std::string>> data
+    ){
+        std::vector<std::string> out;
+
+    };
+  private:
+    std::map<std::string, std::map<std::string, std::string>> elements {
+        { "top", {
+            { "start", "\u250C" },
+            { "middle", "\u252c" },
+            { "line", "\u2500" },
+            { "end", "\u2510" }
+          }
+        },
+        { "center", {
+            { "start", "\u251c" },
+            { "middlei", "\u253c" },
+            { "line", "\u2500" },
+            { "end", "\u2524" }
+        },
+        { "button", {
+            { "start", "\u2514" },
+            { "middle", "\u2534" },
+            { "line", "\u2500" },
+            { "end", "\u2518" }
+        }
+    };
+    std::string element_separator = "\u2502";
+    std::vector<int> calcSize(
+      std::vector<std::vector<std::string>> data
+    ){
+         std::vector<int> out;
+         for (auto & line : data){
+             for (int i = 0 ; i < line.size() ; i++){
+                 if(out[i] < line[i].length())
+                     out[i] = line[i].length();
+             }
+         }
+         return out;
+    };
+    std::string renderLine(
+      std::vector<std::string> columns,
+      std::vector<int> size
+    {
+        std::string out;
+        int line = 0;
+        for(int i = 0 ; i < size.size(); i++){
+            out += element_separator;
+            out += columns[i];
+            out.append(size[i]-columns[i].length(), " ");
+        }
+        out += element_separator;
+        return out;
+    };
+    std::string renderBorder ( 
+      std::vector<int> size,
+      std::string elem_name
+    ){
+        std::string out = this->elements[elem_name]["start"];
+        int line = 0;
+        for(int & i : size){
+            if (line > 0)
+                out += this->elements[elem_name]["middle"];
+            out.append(i," ");
+        }
+        out += this->elements[elem_name]["end"];
+        return out;
+
+    };
+}
+}
